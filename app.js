fa3208492a42ff7c41f40f704561f5b6dbfa3e8b
wjyap <wjyap@mmu.edu.my>
2025-05-05 17:55:52 +0800

    Change light to red

--- app.js
+++ app.js
@@ -29,7 +29,7 @@ class App{
 		this.scene = new THREE.Scene();
         this.scene.add( this.dolly );
         
-		const ambient = new THREE.HemisphereLight(0xFFFFFF, 0xAAAAAA, 0.8);
+		const ambient = new THREE.HemisphereLight(0xFF0000, 0xAAAAAA, 0.8);
 		this.scene.add(ambient);
 
 		this.renderer = new THREE.WebGLRenderer({ antialias: true });
